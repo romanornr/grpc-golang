040415e809c590d98bad19f4de8196a1ce6eca67
romanornr <romanornr@gmail.com>
2019-05-12 18:03:43 +0200

    Test

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
-package grpc_golang
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+)
+
+type Person struct {
+	Name string
+	Age int32
+}
+
+func main() {
+	elliot := &Person {
+		Name: "Elliot",
+		Age: 24,
+	}
+
+	data, err := proto.Marshal(elliot)
+	if err != nil {
+		log.Panic()
+	}
+
+	fmt.Println(data)
+
+	newElliot := &Person{}
+	err = proto.Unmarshal(data, newElliot)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(newElliot.Age)
+}
